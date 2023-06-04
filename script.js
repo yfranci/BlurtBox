1478781c88b9033d7aa138ffaa83ebd1cf37d0b9
Youssef Alan Franci <yfranci@ucsd.edu>
2023-06-03 23:22:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const topicInput = document.getElementById("topic-input");
 const topicHeader = document.getElementById("topic");
 const timer = document.getElementById("timer");
-const resetButton = document.getElementById("reset-button");
+const container = document.getElementById("container");
 
 let time = 300; // 5 minutes in seconds
 let countdownInterval;
+let timerStarted = false;
 
 function startTimer() {
     topicHeader.textContent = topicInput.value;
     topicInput.style.display = "none";
     timer.style.display = "block";
+    container.insertAdjacentHTML('beforeend', '<button id="reset-button">Reset</button>');
+
+    resetButton = document.getElementById("reset-button");
+    resetButton.addEventListener("click", resetTimer);
 
     countdownInterval = setInterval(updateTimer, 1000);
+    timerStarted = true;
 }
 
 function updateTimer() {
@@ -25,6 +31,7 @@ function updateTimer() {
     if (time === 0) {
         clearInterval(countdownInterval);
         timer.textContent = "Time's up!";
+        removeResetButton();
     }
 
     time--;
@@ -36,12 +43,19 @@ function resetTimer() {
     timer.textContent = "5:00";
     topicHeader.textContent = "Enter a Topic";
     topicInput.style.display = "block";
+    removeResetButton();
+    timerStarted = false;
 }
 
-topicInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter" && topicInput.value.trim() !== "") {
+function removeResetButton() {
+    const resetButton = document.getElementById("reset-button");
+    if (resetButton) {
+        resetButton.remove();
+    }
+}
+
+topicInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" && topicInput.value.trim() !== "" && !timerStarted) {
         startTimer();
     }
 });
-
-resetButton.addEventListener("click", resetTimer);
