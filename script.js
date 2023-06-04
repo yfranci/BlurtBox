ba6956539ce2e9b1f54ad900eb37da01a777f184
Youssef Alan Franci <yfranci@ucsd.edu>
2023-06-03 23:59:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
-const topicHeader = document.getElementById("topic");
 const accessCodeContainer = document.getElementById("access-code-container");
 const schoolSelect = document.getElementById("school-select");
 const accessCodeInput = document.getElementById("access-code-input");
 const validateButton = document.getElementById("validate-button");
-const timerContainer = document.getElementById("timer-container");
-const topicInputLabel = document.getElementById("topic-input-label");
+const topicContainer = document.getElementById("topic-container");
 const topicInput = document.getElementById("topic-input");
-const startButton = document.getElementById("start-button");
+const timerContainer = document.getElementById("timer-container");
+const topicHeader = document.getElementById("topic-header");
 const timer = document.getElementById("timer");
+const startButton = document.getElementById("start-button");
 const resetButton = document.getElementById("reset-button");
 
 let time = 300; // 5 minutes in seconds
 let countdownInterval;
 
+function validateAccessCode() {
+    const selectedSchool = schoolSelect.value;
+    const accessCode = accessCodeInput.value;
+
+    let isValid = false;
+
+    if (selectedSchool === "preuss" && accessCode === "k98w4Vyd^") {
+        isValid = true;
+    } else if (selectedSchool === "crawford" && accessCode === "#89iM4OTm") {
+        isValid = true;
+    }
+
+    if (isValid) {
+        accessCodeContainer.style.display = "none";
+        topicContainer.style.display = "block";
+    } else {
+        alert("Incorrect access code. Please try again.");
+    }
+}
+
 function startTimer() {
-    topicHeader.textContent = topicInput.value;
-    accessCodeContainer.style.display = "none";
+    const topic = topicInput.value;
+    if (topic === "") {
+        alert("Please enter a topic.");
+        return;
+    }
+
+    topicHeader.textContent = topic;
+    topicContainer.style.display = "none";
     timerContainer.style.display = "block";
     startButton.style.display = "none";
     resetButton.style.display = "block";
@@ -39,45 +65,16 @@ function updateTimer() {
     time--;
 }
 
-function validateAccessCode() {
-    const selectedSchool = schoolSelect.value;
-    const accessCode = accessCodeInput.value;
-
-    let isValid = false;
-
-    if (selectedSchool === "preuss" && accessCode === "k98w4Vyd^") {
-        isValid = true;
-    } else if (selectedSchool === "crawford" && accessCode === "#89iM4OTm") {
-        isValid = true;
-    }
-
-    if (isValid) {
-        topicInputLabel.style.display = "block";
-        topicInput.style.display = "block";
-        startButton.style.display = "block";
-        validateButton.style.display = "none";
-        accessCodeInput.style.display = "none";
-        schoolSelect.style.display = "none";
-    } else {
-        alert("Incorrect access code. Please try again.");
-    }
-}
-
 function resetTimer() {
     clearInterval(countdownInterval);
     time = 300;
     timer.textContent = "5:00";
     accessCodeInput.value = "";
     topicInput.value = "";
-    topicHeader.textContent = "Enter a Topic";
+    topicHeader.textContent = "";
     accessCodeContainer.style.display = "block";
+    topicContainer.style.display = "none";
     timerContainer.style.display = "none";
-    topicInputLabel.style.display = "none";
-    topicInput.style.display = "none";
-    startButton.style.display = "none";
-    validateButton.style.display = "block";
-    accessCodeInput.style.display = "block";
-    schoolSelect.style.display = "block";
     resetButton.style.display = "none";
 }
 
