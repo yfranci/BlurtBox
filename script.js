bf0adf4138e1bbdae1d549f50925ab5e84ec6755
Youssef Alan Franci <yfranci@ucsd.edu>
2023-06-03 23:30:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const topicInput = document.getElementById("topic-input");
 const topicHeader = document.getElementById("topic");
 const timer = document.getElementById("timer");
-const container = document.getElementById("container");
 
 let time = 300; // 5 minutes in seconds
 let countdownInterval;
@@ -11,10 +10,6 @@ function startTimer() {
     topicHeader.textContent = topicInput.value;
     topicInput.style.display = "none";
     timer.style.display = "block";
-    container.insertAdjacentHTML('beforeend', '<button id="reset-button">Reset</button>');
-
-    resetButton = document.getElementById("reset-button");
-    resetButton.addEventListener("click", resetTimer);
 
     countdownInterval = setInterval(updateTimer, 1000);
     timerStarted = true;
@@ -31,7 +26,7 @@ function updateTimer() {
     if (time === 0) {
         clearInterval(countdownInterval);
         timer.textContent = "Time's up!";
-        removeResetButton();
+        showResetButton();
     }
 
     time--;
@@ -43,14 +38,24 @@ function resetTimer() {
     timer.textContent = "5:00";
     topicHeader.textContent = "Enter a Topic";
     topicInput.style.display = "block";
-    removeResetButton();
+    hideResetButton();
     timerStarted = false;
 }
 
-function removeResetButton() {
+function showResetButton() {
     const resetButton = document.getElementById("reset-button");
     if (resetButton) {
-        resetButton.remove();
+        resetButton.style.display = "block";
+    } else {
+        const container = document.getElementById("container");
+        container.insertAdjacentHTML('beforeend', '<button id="reset-button" onclick="resetTimer()">Reset</button>');
+    }
+}
+
+function hideResetButton() {
+    const resetButton = document.getElementById("reset-button");
+    if (resetButton) {
+        resetButton.style.display = "none";
     }
 }
 
